9dc853960e4ec1374fa24cd73a936babe7cc0c33
Vasilii Stepanov <71067780+va-stepanov@users.noreply.github.com>
2021-05-18 17:41:26 +0300

    Create path.sh

---
+++ path.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export KALDI_ROOT=/opt/kaldi
+#echo ls $KALDI_ROOT
+#ls $KALDI_ROOT
+
+# additional paths for building HCLGa.fst 
+export PATH=$KALDI_ROOT/src/bin:$KALDI_ROOT/src/fstbin:$PATH
+
+export PATH=$KALDI_ROOT/src/latbin:$KALDI_ROOT/src/online2bin:$KALDI_ROOT/src/lmbin:$KALDI_ROOT/src/rnnlmbin:$KALDI_ROOT/egs/wsj/s5/utils:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
+#echo PATH=$PATH
+
+export LC_ALL=C
